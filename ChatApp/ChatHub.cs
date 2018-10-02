96a0ae502f07c2868d24db8ac7ed37eeb863f797
JustinRChou <justinrchou@gmail.com>
2018-10-01 22:07:06 -0700

    Add project files.

---
+++ ChatApp/ChatHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Concurrent;
+
+namespace ChatApp
+{
+    public static class DataStore { 
+        private static ConcurrentQueue<Tuple<string, string>> list = new ConcurrentQueue<Tuple<string, string>>();
+        public static void AddText(string name, string message) {
+            list.Enqueue(Tuple.Create<string, string>(name, message));
+        }
+        public static string GetText() {
+            return JsonConvert.SerializeObject(list);
+        }
+}
+    public class ChatHub : Hub
+    {     
+        public void SendMessage(string name, string message)
+        {
+            DataStore.AddText(name, message);
+            Clients.All.broadcastMessage(name, message);
+        }
+        public void GetAllMessage()
+        {   
+            Clients.All.getAllMessage(DataStore.GetText());
+        }
+    }
+}
---
+++ ChatApp/Scripts/Chat/Chat.js
@@ -0,0 +1,27 @@
+﻿const $name = $("input[name=name]");
+const $message = $("input[name=message]");
+const $chatbox = $("ul[name=chatbox]");
+const chatHub = $.connection.chatHub;
+$.connection.hub.start().done(() => { getAllMessage(); }); 
+chatHub.client.broadcastMessage = function (name, message) {
+    var newMessage = name + ' says: ' + message;
+    $chatbox.append("<li>" + newMessage + "</li>");
+};
+chatHub.client.getAllMessage = function (result) { 
+    const arr = JSON.parse(result);
+    $chatbox.empty();
+    arr.forEach((n, i) => {
+        var newMessage = n.Item1 + ' says: ' + n.Item2;
+        $chatbox.append("<li>" + newMessage + "</li>");
+    });
+};
+newMessage = function () {
+    chatHub.server.sendMessage($name.val(), $message.val());
+};
+getAllMessage = function () {
+    chatHub.server.getAllMessage();
+};
+$(document).ready(() => {
+    $name.val("Guest");
+});
+
